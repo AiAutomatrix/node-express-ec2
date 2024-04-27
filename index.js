92705f160098a68827703f59d27f538b8ec96f04
Garret Friesen <138935870+AiAutomatrix@users.noreply.github.com>
2024-04-27 00:52:34 +0000

    html installed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
 app.listen(5001, () => console.log( ' API running on port 5001'))
 
-app.get('/', (req, res) => res.json( 'My API Running :)'))
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.json( 'My API Running :)'))
